a2bfa387b4092d7e1a03a5e9fc4fe63a5ce1dac7
Marco Quintero <mquintero50@stuy.edu>
2024-10-29 22:25:23 -0400

    Completed syntax error by changing column name from integer to string

--- app/db.py
+++ app/db.py
@@ -4,39 +4,31 @@ import csv       #facilitate CSV I/O
 DB_FILE="onceuponatable.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+latestUID = 0
+latestSID = 0
 
 c.execute("CREATE TABLE userInfo (userID INTEGER, username TEXT, password TEXT);")
 c.execute("CREATE TABLE storyInfo (storyID INTEGER, title TEXT, mainText TEXT, latestEntry TEXT, creator INTEGER);")
-def addUser():
-    c.execute("""
-        INSERT INTO userInfo VALUES
-        (0, 'Maqarov', 'Ghidorah'),
-        (1, 'Maqro Man', 'Gojira')
-    """)
-def addStory():
-    c.execute("""
-        INSERT INTO storyInfo VALUES
-        (0, 'TheBeginning', 'This is the beginning', 'beginning', 'Maqarov'),
-        (1, 'TheEnd', 'This is the end', ' the end', 'Maqro Man')
-    """)
-addUser()
-addStory()
+def addUser(userID, username, password):
+    c.execute(f"INSERT INTO userInfo VALUES({userID}, '{username}', '{password}')")
+def addStory(storyID, title, mainText, latestEntry, creator):
+    c.execute(f"INSERT INTO storyInfo VALUES({storyID}, '{title}', '{mainText}', '{latestEntry}', '{creator}')")
+addUser(latestUID, 'Maqarov', 'Ghidorah')
+addStory(latestSID, 'TheBeginning', 'This is the beginning', 'beginning', 'Maqarov')
 def storiesColumn():
     fin = ""
     storyIDs = c.execute("SELECT storyID FROM storyInfo")
     listIDs = storyIDs.fetchall()
-    print(listIDs)
     if len(listIDs) != 0:
         fin += ", "
     for i in range(len(listIDs)):
-        fin += f'{list(listIDs[i])[0]} BOOLEAN'
+        fin += f'\'{list(listIDs[i])[0]}\' BOOLEAN'
         if i != len(listIDs) - 1:
             fin += ", "
     return fin
-print(storiesColumn())
+#print(storiesColumn())
+#print(f'CREATE TABLE storesContributed (userID INTEGER{storiesColumn()});')
 c.execute(f'CREATE TABLE storesContributed (userID INTEGER{storiesColumn()});')
-latestUID = 0
-latestUID = 0
 
 db.commit()
 db.close()
