b2cb70c918acd5e0d844e9a885c6418c9f1b4771
Marco Quintero <mquintero50@stuy.edu>
2024-11-04 11:01:13 -0500

    End of class

--- app/db.py
+++ app/db.py
@@ -9,33 +9,9 @@ latestSID = -1
 
 c.execute("CREATE TABLE userInfo (userID INTEGER, username TEXT, password TEXT);")
 c.execute("CREATE TABLE storyInfo (storyID INTEGER, title TEXT, mainText TEXT, latestEntry TEXT, creator INTEGER);")
-def addUser(userID, username, password):
-    userID += 1
-    global latestUID
-    latestUID = userID
-    c.execute(f"INSERT INTO userInfo VALUES({userID}, '{username}', '{password}')")
-def addStory(storyID, title, mainText, latestEntry, creator):
-    storyID += 1
-    global latestSID
-    latestSID = storyID
-    c.execute(f"INSERT INTO storyInfo VALUES({storyID}, '{title}', '{mainText}', '{latestEntry}', '{creator}')")
-
-addUser(latestUID, 'Maqarov', 'Ghidorah')
-addStory(latestSID, 'TheBeginning', 'This is the beginning', 'beginning', 'Maqarov')
-
-def storiesColumn():
-    fin = ""
-    storyIDs = c.execute("SELECT storyID FROM storyInfo")
-    listIDs = storyIDs.fetchall()
-    if len(listIDs) != 0:
-        fin += ", "
-    for i in range(len(listIDs)):
-        fin += f'\'{list(listIDs[i])[0]}\' INTEGER'
-        if i != len(listIDs) - 1:
-            fin += ", "
-    print(fin)
-    return fin
-c.execute(f'CREATE TABLE storiesContributed (userID INTEGER{storiesColumn()});')
+c.execute(f'CREATE TABLE storiesContributed (userID INTEGER);')
+def addStoryColumn(storyID):
+    c.execute(f"ALTER TABLE storiesContributed ADD {storyID} INTEGER")
 def addContribs(userID):
     vals = f"INSERT INTO storiesContributed VALUES({userID}"
     if latestSID >= 0:
@@ -47,7 +23,26 @@ def addContribs(userID):
     vals += ")"
     print(vals)
     c.execute(vals)
-addContribs(0)
+def updateContribs(userID, storyID):
+    c.execute(f"UPDATE storiesContributed SET \'{storyID}\' = 1 WHERE userID = {userID}")
+def addUser(userID, username, password):
+    userID += 1
+    global latestUID
+    latestUID = userID
+    c.execute(f"INSERT INTO userInfo VALUES({userID}, '{username}', '{password}')")
+    addContribs(userID)
+def addStory(storyID, title, mainText, latestEntry, creator):
+    storyID += 1
+    global latestSID
+    latestSID = storyID
+    c.execute(f"INSERT INTO storyInfo VALUES({storyID}, '{title}', '{mainText}', '{latestEntry}', '{creator}')")
+    c.execute(f"UPDATE storiesContributed SET \'{storyID}\' = 1 WHERE userID = {creator}")
+    updateContribs(creator, storyID)
+
+addUser(latestUID, 'Maqarov', 'Ghidorah')
+addUser(latestUID, 'Tyson', 'Mike')
+addStory(latestSID, 'TheBeginning', 'This is the beginning', 'beginning', 0)
+addStory(latestSID, 'TheEnd', 'This is the end', 'the end', 1)
 
 
 db.commit()
