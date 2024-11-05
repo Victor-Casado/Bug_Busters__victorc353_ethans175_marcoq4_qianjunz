7ee9fe6a030d37c638d3c2f4eb9b49350b060ef8
Marco Quintero <mquintero50@stuy.edu>
2024-11-05 10:25:47 -0500

    Modified addStoryColumn and addContribs so that unedited stories are marked with 0 instead of null

--- app/db.py
+++ app/db.py
@@ -8,44 +8,38 @@ latestUID = -1
 latestSID = -1
 
 #Creating three tables
-#c.execute("CREATE TABLE userInfo (userID INTEGER, username TEXT, password TEXT);")
-#c.execute("CREATE TABLE storyInfo (storyID INTEGER, title TEXT, mainText TEXT, latestEntry TEXT, creator INTEGER);")
-#c.execute(f'CREATE TABLE storiesContributed (userID INTEGER);')
+c.execute("CREATE TABLE userInfo (userID INTEGER, username TEXT, password TEXT);")
+c.execute("CREATE TABLE storyInfo (storyID INTEGER, title TEXT, mainText TEXT, latestEntry TEXT, creator INTEGER);")
+c.execute(f'CREATE TABLE storiesContributed (userID INTEGER);')
 
 #Functions that add data
 def addStoryColumn(storyID): #Called in the addStory function. Add column to storiesContributed corresponding to latestSID
     c.execute(f"ALTER TABLE storiesContributed ADD \'{storyID}\' INTEGER")
+    c.execute(f"UPDATE storiesContributed SET \'{storyID}\' = 0")
 def addContribs(userID):
-    vals = f"INSERT INTO storiesContributed VALUES({userID}"
-    print ("latestSID: " + str(latestSID))
-    if latestSID >= 0:
-        vals += ", "
-    for i in range(latestSID + 1):
-        vals += "0"
-        if i < latestSID:
-            vals += ", "
-    vals += ")"
-    print("Final execute: " + vals)
+    vals = f"INSERT INTO storiesContributed VALUES({userID})"
+    #print("Final execute for addContribs: " + vals)
     c.execute(vals)
-def updateContribs(userID, storyID): #Called in the addStory function to update data value to a 1. Will also be called in __init__.py when user outside of creator adds something
+def updateContribs(userID, storyID): #Called in the addStory and updateStory functions to update data value to a 1.
     c.execute(f"UPDATE storiesContributed SET \'{storyID}\' = 1 WHERE userID = {userID}")
 def addUser(username, password): #Called by __init__.py when user signs up
     global latestUID
     latestUID += 1
     c.execute(f"INSERT INTO userInfo VALUES({latestUID}, '{username}', '{password}')")
     addContribs(latestUID)
-def addStory(storyID, title, mainText, latestEntry, creator): #Called by __init__.py when new story is created
+def addStory(title, mainText, latestEntry, creator): #Called by __init__.py when new story is created
     global latestSID
     latestSID += 1
-    c.execute(f"INSERT INTO storyInfo VALUES({latestSID}, '{title}', '{mainText}', '{latestEntry}', '{creator}')")
-    addStoryColumn(latestID)
+    c.execute(f"INSERT INTO storyInfo VALUES({latestSID}, '{title}', '{mainText}', '{latestEntry}', {creator})")
+    addStoryColumn(latestSID)
     updateContribs(creator, latestSID)
-def updateStory(storyID, newText): #Called by __init__.py when new user makes an update to a story
+def updateStory(storyID, newText, creator): #Called by __init__.py when new user makes an update to a story
     res = c.execute(f"SELECT mainTEXT FROM storyInfo WHERE storyID = {storyID}")
     mainText = list(res.fetchone())[0]
     #print(mainText)
     newMainText = mainText + newText
-    c.execute(f"UPDATE storyINFO SET latestEntry = '{newText}', mainText = '{newMainText}' WHERE storyID = {storyID}")
+    c.execute(f"UPDATE storyINFO SET latestEntry = '{newText}', mainText = '{newMainText}', creator = {creator} WHERE storyID = {storyID}")
+    updateContribs(creator, storyID)
 
 #Functions that get data. Will all be called by __init__.py directly
 
@@ -56,6 +50,12 @@ def getPassword(userID):
 def getUsername(userID):
     res = c.execute(f"SELECT username FROM userInfo WHERE userID = {userID}")
     return (list(res.fetchone())[0])
+def allUserData():
+    res = c.execute(f"SELECT * FROM userInfo")
+    userList = res.fetchall()
+    for i in range(len(userList)):
+        userList[i] = list(userList[i])
+    return (userList)
 #storyInfo get functions
 def getTitle(storyID):
     res = c.execute(f"SELECT title FROM storyInfo WHERE storyID = {storyID}")
@@ -75,13 +75,14 @@ def hasWritten(userID, storyID): #Will return 1 as an integer or null
     return (list(res.fetchone())[0])
 
 #Test Functions: Will be commented when testing is finished
-#addUser('Maqarov', 'Ghidorah')
-#addUser('Tyson', 'Mike')
-#addStory('TheBeginning', 'This is the beginning', 'beginning', 0)
-#addStory('TheEnd', 'This is the end', 'the end', 1)
-#updateStory(1, ' Hold your breath and count to ten')
-#print(getPassword(0))
-#print(hasWritten(0, 1))
+addUser('Maqarov', 'Ghidorah')
+addUser('Tyson', 'Mike')
+addStory('TheBeginning', 'This is the beginning', 'beginning', 0)
+addStory('TheEnd', 'This is the end', 'the end', 1)
+updateStory(1, ' Hold your breath and count to ten', 1)
+print(getPassword(0))
+print(hasWritten(0, 1))
+print(allUserData())
 
 
 db.commit()
